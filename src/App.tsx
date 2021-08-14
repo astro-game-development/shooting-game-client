7b8e122a144a67a95fc0207c38923459ccd9e463
อภิสิทธื์ ลอมโฮม <frongrider2@gmail.com>
2021-08-14 14:03:21 +0700

    starter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import BoardView from './components/BoardView';
 
 const App = () => {
   return (
     <div className="container">
       <h1>2048</h1>
-      <BoardView />
     </div>
   );
 };
